2f8adc34c0a08162a2f719a12a80f1488cecc244
Annaguzel <you@example.com>
2020-03-25 14:20:04 +0200

    holy guacamole :D

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Home";
+import Game from "./Game";
+function App() {
+  return (
+    <div className="bgcolor">
+      <Switch>
+        <Route exact path="/game" component={Game} />
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </div>
+  );
+}
+export default App;
---
+++ src/Game.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+ 
+class Game extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h2>Game</h2>
+        
+      </div>
+    );
+  }
+}
+ 
+export default Game;
